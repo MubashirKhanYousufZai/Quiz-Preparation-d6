b1e8bd0a9b326b4ebc4b79f1f4eac0648495a783
MubashirKhanYousufZai <161746933+MubashirKhanYousufZai@users.noreply.github.com>
2024-07-21 16:48:15 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+// TUPLES: 
+//             tuples is also called typed array with a predefined length and types for each index.
+let arr;
+arr = ["Mubashir", 8, true];
+arr.push({ name: "Mubashir" });
+console.log(arr);
---
+++ main.ts
@@ -0,0 +1,12 @@
+// TUPLES: 
+//             tuples is also called typed array with a predefined length and types for each index.
+
+let arr: [string, number, boolean, {name:string}?]
+arr = ["Mubashir",8,true]
+
+arr.push({name: "Mubashir"});
+
+console.log(arr);
+
+
+
